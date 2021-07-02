c8d82490ffd60affe6cac5dd5c060e604e608d42
Stanislaw Baranski <stan@stan.bar>
2021-07-02 21:18:53 +0200

    feat: implement full account

--- app/Main.hs
+++ app/Main.hs
@@ -5,42 +5,9 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe
 
-data Transaction = Transaction
-  { from :: String,
-    to :: String,
-    value :: Integer
-  }
-  deriving (Show)
-
-type State = Map String Integer
-
-updateLedger :: State -> Transaction -> State
-updateLedger state tx = do
-  Map.adjust (+ amount) payee $ Map.adjust (subtract amount) payer state
-  where
-    payer = from tx
-    payee = to tx
-    amount = value tx
-
-sigma :: State -> Transaction -> State
-sigma state tx = do
-  case payerBalance of
-    Just currentValue ->
-      if currentValue >= amount
-        then updateLedger state tx
-        else state
-    Nothing -> state
-  where
-    payerBalance = Map.lookup payer state
-    payer = from tx
-    amount = value tx
-
-stateTransition :: State -> [Transaction] -> State
-stateTransition = foldl sigma
-
 main :: IO ()
 main = do
-  let myState = Map.fromList [("stasbar", 5), ("gbaranski", 4)]
+  let myState = Map.fromList [("stasbar", withBalance 5), ("gbaranski", withBalance 4)]
       transactions =
         [ Transaction {from = "stasbar", to = "gbaranski", value = 6},
           Transaction {from = "stasbar", to = "gbaranski", value = 4},
@@ -50,3 +17,54 @@ main = do
         ]
   let finalState = stateTransition myState transactions
   print finalState
+
+data Transaction = Transaction
+  { from :: String,
+    to :: String,
+    value :: Integer
+  }
+  deriving (Show)
+
+data Account = Account
+  { nonce :: Integer,
+    balance :: Integer,
+    storageRoot :: Integer,
+    codeHash :: Integer
+  }
+  deriving (Show)
+
+withBalance balance = Account {nonce = 0, balance = balance, storageRoot = 0, codeHash = 0}
+
+type State = Map String Account
+
+updateBalance :: (Integer -> Integer) -> Account -> Account
+updateBalance adjuster acc = do
+  Account
+    { nonce = nonce acc,
+      balance = adjuster $ balance acc,
+      storageRoot = storageRoot acc,
+      codeHash = codeHash acc
+    }
+
+updateLedger :: State -> Transaction -> State
+updateLedger state tx = do
+  Map.adjust (updateBalance (+ amount)) payee $ Map.adjust (updateBalance (subtract amount)) payer state
+  where
+    payer = from tx
+    payee = to tx
+    amount = value tx
+
+sigma :: State -> Transaction -> State
+sigma state tx = do
+  case Map.lookup payer state of
+    Just payerAccount ->
+      if amount <= balance payerAccount
+        then updateLedger state tx
+        else state
+    Nothing -> state
+  where
+    payer = from tx
+    amount = value tx
+
+stateTransition :: State -> [Transaction] -> State
+stateTransition = foldl sigma
