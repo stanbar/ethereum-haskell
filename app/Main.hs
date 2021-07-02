3619959bc247dcc75cd52a282360b48f08e89863
Stanislaw Baranski <stan@stan.bar>
2021-07-02 16:21:35 +0200

    feat: fix update ledger function

--- app/Main.hs
+++ app/Main.hs
@@ -5,31 +5,34 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe
 
-data Transaction = Transaction { from :: String
-  , to :: String
-    , value :: Integer
-} deriving Show
-
+data Transaction = Transaction
+  { from :: String,
+    to :: String,
+    value :: Integer
+  }
+  deriving (Show)
 
 type State = Map String Integer
 
-actualUpdate :: Maybe a -> payer -> State -> State
-actualUpdate f payer state = Map.update f payer state
-
 updateLedger :: State -> Transaction -> State
-updateLedger state tx = actualUpdate f payer state
-    where f currentValue = if currentValue >= amount then Just $ currentValue - amount else Nothing
-          payer = from tx
-          amount = value tx
+updateLedger state tx = do
+  let state' = Map.update accountPayer payer state
+  Map.update accountPayee payee state'
+  where
+    accountPayer currentValue = if currentValue >= amount then Just $ currentValue - amount else Nothing
+    accountPayee currentValue = if currentValue >= amount then Just $ currentValue + amount else Nothing
+    payer = from tx
+    payee = to tx
+    amount = value tx
 
 sigma :: State -> [Transaction] -> State
 sigma = foldl updateLedger
 
+-- sigma state txs -> do
 
--- sigma state txs -> do  
-
-main :: IO (State)
+main :: IO ()
 main = do
   let myState = Map.fromList [("stasbar", 5), ("gbaranski", 4)]
-      transactions = [Transaction {from="stasbar", to="gbaranski", value=6}, Transaction {from="stasbar", to="gbaranski", value=4}]
-  return $ sigma myState transactions
+      transactions = [Transaction {from = "stasbar", to = "gbaranski", value = 6}, Transaction {from = "stasbar", to = "gbaranski", value = 4}]
+  let finalState = sigma myState transactions
+  print finalState
