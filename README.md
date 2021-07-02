26cdf080d27ba7b85512a5dbabcd0e8891a939ea
Stanislaw Baranski <stan@stan.bar>
2021-07-02 20:45:11 +0200

    feat: finish state transition function

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ethereum-haskell
+# Ethereum-Haskell
--- app/Main.hs
+++ app/Main.hs
@@ -16,23 +16,37 @@ type State = Map String Integer
 
 updateLedger :: State -> Transaction -> State
 updateLedger state tx = do
-  let state' = Map.update accountPayer payer state
-  Map.update accountPayee payee state'
+  Map.adjust (+ amount) payee $ Map.adjust (subtract amount) payer state
   where
-    accountPayer currentValue = if currentValue >= amount then Just $ currentValue - amount else Nothing
-    accountPayee currentValue = if currentValue >= amount then Just $ currentValue + amount else Nothing
     payer = from tx
     payee = to tx
     amount = value tx
 
-sigma :: State -> [Transaction] -> State
-sigma = foldl updateLedger
+sigma :: State -> Transaction -> State
+sigma state tx = do
+  case payerBalance of
+    Just currentValue ->
+      if currentValue >= amount
+        then updateLedger state tx
+        else state
+    Nothing -> state
+  where
+    payerBalance = Map.lookup payer state
+    payer = from tx
+    amount = value tx
 
--- sigma state txs -> do
+stateTransition :: State -> [Transaction] -> State
+stateTransition = foldl sigma
 
 main :: IO ()
 main = do
   let myState = Map.fromList [("stasbar", 5), ("gbaranski", 4)]
-      transactions = [Transaction {from = "stasbar", to = "gbaranski", value = 6}, Transaction {from = "stasbar", to = "gbaranski", value = 4}]
-  let finalState = sigma myState transactions
+      transactions =
+        [ Transaction {from = "stasbar", to = "gbaranski", value = 6},
+          Transaction {from = "stasbar", to = "gbaranski", value = 4},
+          Transaction {from = "stasbar", to = "gbaranski", value = 2},
+          Transaction {from = "stasbar", to = "gbaranski", value = 1},
+          Transaction {from = "gbaranski", to = "stasbar", value = 9}
+        ]
+  let finalState = stateTransition myState transactions
   print finalState
